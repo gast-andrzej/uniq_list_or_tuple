a7bf7f738f2a6e689c5122ea2d2c2d1ca613bf01
gast-andrzej <71961055+gast-andrzej@users.noreply.github.com>
2024-05-09 20:11:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,3 +23,17 @@ def func_gener_uniq_list():
 
     for i in q: open('texter.txt', 'a').write(tu_moduj_skladnie + f"{list(i)}" + opcjonalne_zamkniecie_warunku + '\n')
 func_gener_uniq_list()
+
+
+def func_gener_uniq_list_kamil():
+    ilosc_kombinacji = 2**18 #ile unikatów chcesz wygenerować -> format int
+
+    q = set()
+    z = 1
+    while len(q) < ilosc_kombinacji:
+        q.add(tuple(__import__('random').randint(0,1) for z in range(18)))
+
+    for i in q:
+        open('texter.txt', 'a').write(f"else if (num == {z})"+"{"+f"{list(i)}" + "}"+'\n')
+        z+=1
+func_gener_uniq_list_kamil()
